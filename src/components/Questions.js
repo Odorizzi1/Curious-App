2e2d617e0c2d5c1f1f8fa9f6746d49703dc35411
jonathan <jonathan.odorizzi@clinicorp.com>
2023-02-28 11:29:26 -0300

    Update Questions.js

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -6,7 +6,33 @@ let teste = [
     },
     {
         name: "teste2"
-    }
+    },
+    {
+        name: "teste3"
+    },
+    {
+        name: "teste4"
+    }, {
+        name: "teste5"
+    },
+    {
+        name: "teste6"
+    }, {
+        name: "teste7"
+    },
+    {
+        name: "teste8"
+    }, {
+        name: "teste9"
+    },
+    {
+        name: "teste10"
+    }, {
+        name: "teste11"
+    },
+    {
+        name: "teste12"
+    },
 ]
 
 
