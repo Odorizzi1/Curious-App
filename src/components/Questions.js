f64087b682cfc4dd924d978c40c255f7553817ac
jonathan <jonathan.odorizzi@clinicorp.com>
2023-02-28 21:52:54 -0300

    Task-[3]Paginação

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,31 +3,48 @@ import axios from "axios";
 
 const Questions = ({ name }) => {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [maxTimestamp, setMaxTimestamp] = useState(null);
 
     useEffect(() => {
-        list()
-    }, [])
+        list();
+    }, []);
 
     function list() {
-        return axios.get('https://curiouscat.live/api/v2.1/profile?username=zanfranceschi').then(res => {
-            let resultData = res.data.posts;
-            let resultPost = resultData.map(res => res.post)
-            setData(resultPost)
-        })
+        let url = "https://curiouscat.live/api/v2.1/profile?username=zanfranceschi";
+        if (maxTimestamp !== null) {
+            url += `&max_timestamp=${maxTimestamp}`;
+        }
+        return axios
+            .get(url)
+            .then(res => {
+                let resultData = res.data.posts;
+                let resultPost = resultData.map(res => res.post);
+                setMaxTimestamp(resultData[resultData.length - 1].post.timestamp);
+                setData(resultData => [...resultData, ...resultPost.slice(1)]); // Remove o primeiro item
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+
+    function handleLoadMore() {
+        list();
     }
 
     return (
         <div className="question-content">
             {data?.map(res => {
                 return (
-                    <div className="questions-list">
+                    <div className="questions-list" key={res.id}>
                         {res.comment}
                     </div>
-                )
+                );
             })}
+            <button onClick={handleLoadMore}>Load more</button>
         </div>
-    )
-}
+    );
+};
 
 export default Questions;
